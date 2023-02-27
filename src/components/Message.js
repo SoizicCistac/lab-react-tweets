d48f0d35792be5cee799b9bc1b32e2bc289f5c5a
Soizic Cistac <soizic.cistac@gmail.com>
2023-02-27 16:10:59 +0100

    done

---
+++ src/components/Message.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Message = (props) => {
+  return (
+    <>
+      <p className="message">
+          {props.message}
+        </p>
+    </>
+  )
+}
+
+export default Message
